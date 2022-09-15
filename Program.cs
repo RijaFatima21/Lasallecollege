b5429e77d68d542b8fa19c55a19780b33983f416
unknown <rjfatima21@gmail.com>
2022-09-15 13:14:43 -0400

    first comment

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*int x=5;
+double y=(double)x;
+Console.WriteLine(y);
+float z=10.34f;
+int a=(int)z;
+Console.WriteLine(a);
+Console.WriteLine("Enter the first numbers to add:");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the second number to add");
+int num2=Convert.ToInt32(Console.ReadLine());
+int sum=num1+num2;
+Console.WriteLine("sum is:"+sum);*/
+int x=5;
+int y=10;
+Console.WriteLine(x==y);
+Console.WriteLine("enter the first number");
+x=Convert.ToInt32(Console.ReadLine());
+
+
+
+
